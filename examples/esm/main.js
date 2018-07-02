2de7bfc7b888c0df0099321a51b1075aef6a2e4c
Mattias Wallander <mattias@wallander.eu>
2018-07-02 19:26:01 +0200

    Add examples for AMD and native ES modules

---
+++ examples/esm/main.js
@@ -0,0 +1,53 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+// The UMD format used in the pre-built file in the dist folder won't work with
+// native ES modules. Just use the main source file instead since the project is
+// already written as ES modules.
+import ExifReader from '../../src/exif-reader';
+
+if (supportsFileReader()) {
+    document.getElementById('file').addEventListener('change', handleFile, false);
+} else {
+    document.write('<strong>Sorry, your web browser does not support the FileReader API.</strong>');
+}
+
+function supportsFileReader() {
+    return window.FileReader !== undefined;
+}
+
+function handleFile(event) {
+    const files = event.target.files;
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+        try {
+            const tags = ExifReader.load(event.target.result);
+
+            // The MakerNote tag can be really large. Remove it to lower
+            // memory usage if you're parsing a lot of files and saving the
+            // tags.
+            delete tags['MakerNote'];
+
+            listTags(tags);
+        } catch (error) {
+            alert(error);
+        }
+    };
+
+    // We only need the start of the file for the Exif info.
+    reader.readAsArrayBuffer(files[0].slice(0, 128 * 1024));
+}
+
+function listTags(tags) {
+    const tableBody = document.getElementById('exif-table-body');
+    for (const name in tags) {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${name}</td>
+            <td>${tags[name].description}</td>
+        `;
+        tableBody.appendChild(row);
+    }
+}
