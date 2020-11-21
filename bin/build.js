4c1d1b3d53096b4bc08f65a8e567d238cf84e04b
Mattias Wallander <mattias@wallander.eu>
2020-11-21 16:08:22 +0100

    Make sure dev deps are always installed during custom build

--- bin/build.js
+++ bin/build.js
@@ -9,14 +9,14 @@ const dependentHasExifReaderConfig = require('./findDependentConfig');
 process.chdir(path.join(__dirname, '..'));
 
 if (!process.argv.includes('--only-with-config') || checkConfig()) {
-    execSync('npx webpack', {stdio: 'inherit'});
+    execSync('webpack', {stdio: 'inherit'});
 }
 
 function checkConfig() {
     if (dependentHasExifReaderConfig()) {
         if (!isDependenciesInstalled()) {
             console.log('Installing ExifReader custom build dependencies...'); // eslint-disable-line no-console
-            execSync('npm install --loglevel=error --no-optional --no-package-lock --no-save @babel/core @babel/preset-env @babel/register babel-loader cross-env string-replace-loader webpack webpack-cli terser-webpack-plugin', {stdio: 'inherit'});
+            execSync('npm install --production=false --loglevel=error --no-optional --no-package-lock --no-save @babel/core@7.9.0 @babel/preset-env@7.9.0 @babel/register@7.9.0 babel-loader@8.1.0 cross-env@5.2.0 string-replace-loader@2.2.0 webpack@4.44.1 webpack-cli@3.3.12 terser-webpack-plugin@2.3.8', {stdio: 'inherit'});
             console.log('Done.'); // eslint-disable-line no-console
         }
         return true;
