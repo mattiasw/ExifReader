4c762b9471406298e00619165b068fde4f7198e0
Mattias Wallander <mattias@wallander.eu>
2021-03-26 22:51:28 +0100

    Upgrade dependencies

--- bin/build.js
+++ bin/build.js
@@ -16,7 +16,18 @@ function checkConfig() {
     if (dependentHasExifReaderConfig()) {
         if (!isDependenciesInstalled()) {
             console.log('Installing ExifReader custom build dependencies...'); // eslint-disable-line no-console
-            execSync('npm install --production=false --loglevel=error --no-optional --no-package-lock --no-save @babel/core@7.9.0 @babel/preset-env@7.9.0 @babel/register@7.9.0 babel-loader@8.1.0 cross-env@5.2.0 string-replace-loader@2.2.0 webpack@4.44.1 webpack-cli@3.3.12 terser-webpack-plugin@2.3.8', {stdio: 'inherit'});
+            const packages = [
+                '@babel/core@7.13.10',
+                '@babel/preset-env@7.13.12',
+                '@babel/register@7.13.8',
+                'babel-loader@8.2.2',
+                'cross-env@7.0.3',
+                'string-replace-loader@2.2.0',
+                'webpack@4.44.1',
+                'webpack-cli@3.3.12',
+                'terser-webpack-plugin@4.2.3'
+            ];
+            execSync(`npm install --production=false --loglevel=error --no-optional --no-package-lock --no-save ${packages.join(' ')}`, {stdio: 'inherit'});
             console.log('Done.'); // eslint-disable-line no-console
         }
         return true;
