d8ef463e68ea29ae5f3b6f79912bf9ecee10423a
Mattias Wallander <mattias@wallander.eu>
2023-11-18 19:55:32 +0100

    Make sure no extra packages are installed when making a custom build
    `npm install` always installs the packages in `package.json` if they are
    not already in `node_modules`. There is no flag to npm for avoiding
    this. Therefore we use a temporary directory for installing the
    necessary packages for making the custom build.
    
    #262

--- bin/build.js
+++ bin/build.js
@@ -3,12 +3,14 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 const path = require('path');
+const fs = require('fs');
 const {execSync} = require('child_process');
 const dependentHasExifReaderConfig = require('./findDependentConfig');
 
 const WEBPACK_VERSION = '5.89.0';
 
-process.chdir(path.join(__dirname, '..'));
+const EXIFREADER_ROOT_DIR = path.join(__dirname, '..');
+process.chdir(EXIFREADER_ROOT_DIR);
 
 if (!process.argv.includes('--only-with-config') || checkConfig()) {
     execSync(`npx webpack@${WEBPACK_VERSION}`, {stdio: 'inherit'});
@@ -29,8 +31,16 @@ function checkConfig() {
                 'webpack-cli@5.1.4',
                 'terser-webpack-plugin@5.3.9'
             ];
-            execSync(`npm install --production=false --loglevel=error --no-optional --no-package-lock --no-save ${packages.join(' ')}`, {stdio: 'inherit'});
-            console.log('Done.'); // eslint-disable-line no-console
+            const tmpDir = path.join(EXIFREADER_ROOT_DIR, '__tmp');
+            try {
+                initTmpDir(tmpDir);
+                execSync(`npm install --production --loglevel=error --no-optional --no-package-lock --no-save ${packages.join(' ')}`, {stdio: 'inherit'});
+                console.log('Done.'); // eslint-disable-line no-console
+            } catch (error) {
+                console.error('Could not install requirements for a custom build:', error); // eslint-disable-line no-console
+            } finally {
+                cleanUpTmpDir(tmpDir);
+            }
         }
         return true;
     }
@@ -44,4 +54,23 @@ function areDependenciesInstalled() {
     } catch (error) {
         return false;
     }
+}
+
+function initTmpDir(tmpDir) {
+    const nodeModulesDir = path.join(EXIFREADER_ROOT_DIR, 'node_modules');
+    fs.mkdirSync(tmpDir, {recursive: true});
+    if (fs.existsSync(nodeModulesDir)) {
+        fs.renameSync(nodeModulesDir, path.join(tmpDir, 'node_modules'));
+    }
+    process.chdir(tmpDir);
+    execSync('npm init -y', {stdio: 'inherit'});
+}
+
+function cleanUpTmpDir(tmpDir) {
+    const nodeModulesDir = path.join(tmpDir, 'node_modules');
+    if (fs.existsSync(nodeModulesDir)) {
+        fs.renameSync(nodeModulesDir, path.join(EXIFREADER_ROOT_DIR, 'node_modules'));
+    }
+    process.chdir(EXIFREADER_ROOT_DIR);
+    fs.rmSync(tmpDir, {recursive: true, force: true});
 }
