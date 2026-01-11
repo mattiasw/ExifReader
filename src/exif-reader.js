ca2036032b065e9f9a6d2f246209f522bdc5f971
Mattias Wallander <mattias@wallander.eu>
2026-01-11 15:16:07 +0100

    Extract loadView pipeline helpers into module

--- src/exif-reader.js
+++ src/exif-reader.js
@@ -32,6 +32,7 @@ import GifFileTags from './gif-file-tags.js';
 import Thumbnail from './thumbnail.js';
 import Composite from './composite.js';
 import {createTagFilter} from './tag-filter.js';
+import {buildTagsFromMergeSteps, isThenable} from './loadview-pipeline.js';
 import exifErrors from './errors.js';
 
 export default {
@@ -710,6 +711,17 @@ export function loadView(
         throw new exifErrors.MetadataMissingError();
     }
 
+    const pipelineDependencies = {
+        objectAssign,
+        hasPngTextData,
+        filterTagsForParse,
+        filterTagsForReturn,
+        getGpsGroupFromExifTags,
+        Constants,
+        Composite,
+        Thumbnail,
+    };
+
     if (async) {
         if (typeof Promise === 'undefined') {
             throw new Error('Promise is required when async mode is enabled.');
@@ -728,6 +740,7 @@ export function loadView(
                 pngTextChunks,
                 pngTextIsAsync,
                 thumbnailIfdTags,
+                deps: pipelineDependencies,
             });
 
             return tags;
@@ -746,6 +759,7 @@ export function loadView(
         pngTextChunks,
         pngTextIsAsync,
         thumbnailIfdTags,
+        deps: pipelineDependencies,
     });
 
     function shouldReadFileTagsForFileTypeOnlyOptimization(includeTagsOptions) {
@@ -778,393 +792,6 @@ export function loadView(
 
         return !isFileTypeOnly;
     }
-}
-
-function buildTagsFromMergeSteps({
-    mergeSteps,
-    deferredResults,
-    parsedGroups,
-    expanded,
-    tagFilter,
-    dataView,
-    tiffHeaderOffset,
-    fileType,
-    pngTextChunks,
-    pngTextIsAsync,
-    thumbnailIfdTags,
-}) {
-    let tags = {};
-
-    for (let i = 0; i < mergeSteps.length; i++) {
-        tags = applyMergeStep({
-            step: mergeSteps[i],
-            deferredResults,
-            parsedGroups,
-            expanded,
-            tagFilter,
-            dataView,
-            tiffHeaderOffset,
-            fileType,
-            thumbnailIfdTags,
-            tags,
-        });
-    }
-
-    if (
-        expanded
-        && pngTextIsAsync
-        && tagFilter.shouldReturnGroup('png')
-        && tags.png
-    ) {
-        tags.pngText = objectAssign({}, tags.png);
-    }
-
-    if (
-        expanded
-        && tagFilter.shouldReturnGroup('png')
-        && hasPngTextData(pngTextChunks)
-        && tags.png
-        && !tags.pngText
-    ) {
-        tags.pngText = objectAssign({}, tags.png);
-    }
-
-    return tags;
-}
-
-function applyMergeStep({
-    step,
-    deferredResults,
-    parsedGroups,
-    expanded,
-    tagFilter,
-    dataView,
-    tiffHeaderOffset,
-    fileType,
-    thumbnailIfdTags,
-    tags,
-}) {
-    if (step.type === 'mergeGroupAssign') {
-        const returnedTags = filterTagsForReturn(step.groupKey, step.parsedTags, tagFilter);
-
-        return mergeAssignGroup(tags, step.groupKey, returnedTags, expanded);
-    }
-
-    if (step.type === 'mergeGroupMerge') {
-        const returnedTags = filterTagsForReturn(step.groupKey, step.parsedTags, tagFilter);
-
-        return mergeMergeGroup(tags, step.groupKey, returnedTags, expanded);
-    }
-
-    if (step.type === 'mergeXmpGroupAssign') {
-        const returnedTags = filterTagsForReturn('xmp', step.parsedTags, tagFilter);
-
-        if (expanded) {
-            tags.xmp = returnedTags;
-            return tags;
-        }
-
-        const returnedTagsForFlat = objectAssign({}, returnedTags);
-        delete returnedTagsForFlat._raw;
-
-        return objectAssign({}, tags, returnedTagsForFlat);
-    }
-
-    if (step.type === 'mergeIccDeferred') {
-        const resolvedReadTags = deferredResults[step.deferredKey];
-        const parsedIccTags = filterTagsForParse('icc', resolvedReadTags, tagFilter);
-        parsedGroups.icc = parsedIccTags;
-
-        if (!tagFilter.shouldReturnGroup('icc')) {
-            return tags;
-        }
-
-        const returnedIccTags = filterTagsForReturn('icc', parsedIccTags, tagFilter);
-
-        return mergeAssignGroup(tags, 'icc', returnedIccTags, expanded);
-    }
-
-    if (step.type === 'mergePngFile') {
-        const returnedPngFileTags = filterTagsForReturn('png', step.parsedTags, tagFilter);
-
-        if (!tagFilter.shouldReturnGroup('png')) {
-            return tags;
-        }
-
-        if (expanded) {
-            tags.png = !tags.png ? returnedPngFileTags : objectAssign(
-                {},
-                tags.png,
-                returnedPngFileTags
-            );
-            tags.pngFile = returnedPngFileTags;
-
-            return tags;
-        }
-
-        return objectAssign({}, tags, returnedPngFileTags);
-    }
-
-    if (step.type === 'mergePngChunk') {
-        const returnedPngChunkTags = filterTagsForReturn('png', step.parsedTags, tagFilter);
-
-        if (!tagFilter.shouldReturnGroup('png')) {
-            return tags;
-        }
-
-        if (expanded) {
-            tags.png = !tags.png ? returnedPngChunkTags : objectAssign(
-                {},
-                tags.png,
-                returnedPngChunkTags
-            );
-
-            return tags;
-        }
-
-        return objectAssign({}, tags, returnedPngChunkTags);
-    }
-
-    if (step.type === 'processPngTextReadTags') {
-        return addPngTextReadTagsToTagsAndGroups({
-            readTags: step.readTags,
-            parsedGroups,
-            expanded,
-            tagFilter,
-            tags,
-        });
-    }
-
-    if (step.type === 'processPngTextReadTagsDeferredList') {
-        const tagList = deferredResults[step.deferredKey] || [];
-
-        for (let i = 0; i < tagList.length; i++) {
-            tags = addPngTextReadTagsToTagsAndGroups({
-                readTags: tagList[i],
-                parsedGroups,
-                expanded,
-                tagFilter,
-                tags,
-            });
-        }
-
-        return tags;
-    }
-
-    if (step.type === 'gps') {
-        if (
-            expanded
-            && tagFilter.shouldReturnGroup('gps')
-            && parsedGroups.exif
-        ) {
-            const gpsGroup = getGpsGroupFromExifTags(parsedGroups.exif);
-            if (gpsGroup) {
-                const returnedGpsGroup = filterTagsForReturn('gps', gpsGroup, tagFilter);
-                tags.gps = returnedGpsGroup;
-            }
-        }
-
-        return tags;
-    }
-
-    if (step.type === 'composite') {
-        if (!tagFilter.shouldReturnGroup('composite')) {
-            return tags;
-        }
-
-        let compositeInput = tags;
-        let compositeInputExpanded = expanded;
-        if (tagFilter.isActive) {
-            compositeInput = {exif: parsedGroups.exif, file: parsedGroups.file};
-            compositeInputExpanded = true;
-        }
-
-        const composite = Composite.get(compositeInput, compositeInputExpanded);
-        if (!composite) {
-            return tags;
-        }
-
-        const returnedCompositeTags =
-            filterTagsForReturn('composite', composite, tagFilter);
-
-        return mergeAssignGroup(tags, 'composite', returnedCompositeTags, expanded);
-    }
-
-    if (step.type === 'thumbnail') {
-        if (
-            !tagFilter.shouldReturnGroup('thumbnail')
-            || !tagFilter.shouldReturnTag('thumbnail', 'Thumbnail')
-        ) {
-            delete tags.Thumbnail;
-
-            return tags;
-        }
-
-        if (!thumbnailIfdTags) {
-            return tags;
-        }
-
-        const parsedThumbnailIfdTags = thumbnailIfdTags ? filterTagsForParse(
-            'thumbnail',
-            thumbnailIfdTags,
-            tagFilter
-        ) : undefined;
-        if (parsedThumbnailIfdTags) {
-            parsedGroups.thumbnail = parsedThumbnailIfdTags;
-        }
-
-        const thumbnail = (Constants.USE_JPEG || Constants.USE_WEBP)
-            && Constants.USE_EXIF
-            && Constants.USE_THUMBNAIL
-            && Thumbnail.get(dataView, parsedThumbnailIfdTags, tiffHeaderOffset);
-        if (thumbnail) {
-            tags.Thumbnail = thumbnail;
-        } else {
-            delete tags.Thumbnail;
-        }
-
-        return tags;
-    }
-
-    if (step.type === 'fileType') {
-        if (
-            fileType
-            && tagFilter.shouldReturnGroup('file')
-            && tagFilter.shouldReturnTag('file', 'FileType')
-        ) {
-            if (expanded) {
-                if (!tags.file) {
-                    tags.file = {};
-                }
-                tags.file.FileType = fileType;
-            } else {
-                tags.FileType = fileType;
-            }
-        }
-
-        return tags;
-    }
-
-    return tags;
-}
-
-function mergeAssignGroup(tags, groupKey, returnedTags, expanded) {
-    if (expanded) {
-        tags[groupKey] = returnedTags;
-
-        return tags;
-    }
-
-    return objectAssign({}, tags, returnedTags);
-}
-
-function mergeMergeGroup(tags, groupKey, returnedTags, expanded) {
-    if (expanded) {
-        if (!tags[groupKey]) {
-            tags[groupKey] = returnedTags;
-        } else {
-            tags[groupKey] = objectAssign({}, tags[groupKey], returnedTags);
-        }
-
-        return tags;
-    }
-
-    return objectAssign({}, tags, returnedTags);
-}
-
-function addPngTextReadTagsToTagsAndGroups({readTags, parsedGroups, expanded, tagFilter, tags}) {
-    const embeddedExifTags = readTags.__exif;
-    const embeddedIptcTags = readTags.__iptc;
-    delete readTags.__exif;
-    delete readTags.__iptc;
-
-    if (embeddedExifTags) {
-        const parsedEmbeddedExifTags =
-            filterTagsForParse('exif', embeddedExifTags, tagFilter);
-        parsedGroups.exif = !parsedGroups.exif ? parsedEmbeddedExifTags : objectAssign(
-            {},
-            parsedGroups.exif,
-            parsedEmbeddedExifTags
-        );
-
-        if (tagFilter.shouldReturnGroup('exif')) {
-            const returnedEmbeddedExifTags = filterTagsForReturn(
-                'exif',
-                parsedEmbeddedExifTags,
-                tagFilter
-            );
-            if (expanded) {
-                tags.exif = !tags.exif ? returnedEmbeddedExifTags : objectAssign(
-                    {},
-                    tags.exif,
-                    returnedEmbeddedExifTags
-                );
-            } else {
-                tags = objectAssign({}, tags, returnedEmbeddedExifTags);
-            }
-        }
-    }
-
-    if (embeddedIptcTags) {
-        const parsedEmbeddedIptcTags =
-            filterTagsForParse('iptc', embeddedIptcTags, tagFilter);
-        parsedGroups.iptc = !parsedGroups.iptc ? parsedEmbeddedIptcTags : objectAssign(
-            {},
-            parsedGroups.iptc,
-            parsedEmbeddedIptcTags
-        );
-
-        if (tagFilter.shouldReturnGroup('iptc')) {
-            const returnedEmbeddedIptcTags = filterTagsForReturn(
-                'iptc',
-                parsedEmbeddedIptcTags,
-                tagFilter
-            );
-            if (expanded) {
-                tags.iptc = !tags.iptc ? returnedEmbeddedIptcTags : objectAssign(
-                    {},
-                    tags.iptc,
-                    returnedEmbeddedIptcTags
-                );
-            } else {
-                tags = objectAssign({}, tags, returnedEmbeddedIptcTags);
-            }
-        }
-    }
-
-    if (tagFilter.shouldReturnGroup('png')) {
-        const parsedPngTextTags = filterTagsForParse('png', readTags, tagFilter);
-        const returnedPngTextTags =
-            filterTagsForReturn('png', parsedPngTextTags, tagFilter);
-        parsedGroups.pngText = parsedPngTextTags;
-
-        if (expanded) {
-            tags.png = !tags.png ? returnedPngTextTags : objectAssign(
-                {},
-                tags.png,
-                returnedPngTextTags
-            );
-            // Historical behavior in build fixtures:
-            // - if PNG text chunks yield actual "png" tags, `pngText` should contain only those
-            // - otherwise (e.g. only embedded Exif/IPTC), `pngText` should represent the full `png` group
-            //   and will be filled in later as a fallback from `tags.png`
-            if (returnedPngTextTags && Object.keys(returnedPngTextTags).length > 0) {
-                tags.pngText = !tags.pngText ? returnedPngTextTags : objectAssign(
-                    {},
-                    tags.pngText,
-                    returnedPngTextTags
-                );
-            }
-        } else {
-            tags = objectAssign({}, tags, returnedPngTextTags);
-        }
-    }
-
-    return tags;
-}
-
-function isThenable(value) {
-    return !!value && typeof value.then === 'function';
 }
 
 function filterTagsForParse(groupKey, readTags, tagFilter) {
